c51c6717406ae54e5502683773a7d16817e85299
crGowen <39204053+crGowen@users.noreply.github.com>
2022-05-21 10:44:39 +0100

    refined copying of .h and .dll files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { exec } from 'child_process';
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 
 type BuildProfile = {
     cppDirs: string[];
@@ -35,19 +35,22 @@ function run(dryRun: boolean): void {
     const lib:string = build.lib.replaceAll("/", "\\");
     const links:string = lib ? lib + "\\*.lib" : "";
 
-    const dlls:string = build.dll.replaceAll("/", "\\");
+    const dlls:string = build.dll.replaceAll("/", "\\")
+        .split("\\")
+        .slice(0, -1)
+        .join()
+        + "\\*.dll";
         
     const include:string = build.include.replaceAll("/", "\\");
 
-    const hFile:string = build.hFile.replaceAll("/", "\\");
+    const hRaw:string = build.hFile && readFileSync(build.hFile, 'utf8');
+
+    const hFile = hRaw.replaceAll('__declspec(dllexport)', '__declspec(dllimport)');
 
     const out:string = build.output.replaceAll("/", "\\");
 
-    const res = generateCommand(cpps, include, links, dlls, flags, out, hFile);
+    runCommand(cpps, include, links, dlls, flags, out, hFile, dryRun);
 
-    dryRun
-        ? console.log(res)
-        : exec(res, (_, stdout) => console.log(stdout));
 };
 
 function verifyBuildFile(buildFile: BuildProfile): buildFile is BuildProfile {
@@ -65,16 +68,26 @@ function verifyBuildFile(buildFile: BuildProfile): buildFile is BuildProfile {
         ]);
 };
 
-function generateCommand(cpps: string, include:string, links:string, dlls:string, flags: string, out:string, hFile:string): string {
+function runCommand(cpps: string, include:string, links:string, dlls:string, flags: string, out:string, hFile:string, dryRun: boolean): void {
     const outDir = out.split("\\").slice(0, -1).join('\\');
+    const outName = out.split("\\").slice(-1).join('').split('.')[0];
     const filetypeRaw = out.split(".").slice(-1)[0];
     const fileType = filetypeRaw === 'exe' || filetypeRaw === 'dll' ? filetypeRaw : 'exe';
 
     const includeCmd = include ? `/I ${include}` : "";
     const dllFlag = fileType === 'dll' ? "/LD" : "";
     const cleanUpDllStuff = dllFlag ? `& cd ${outDir} && del *.exp` : ""; 
-    const copyHeadCmd = hFile ? `&& echo D | xcopy /y ${hFile} ${outDir}` : "";
     const copyDllsCmd = dlls ? `&& echo D | xcopy /y ${dlls} ${outDir}` : "";
 
-    return `mkdir ${outDir} & cl ${cpps} ${links} ${includeCmd} ${flags} ${dllFlag} /Fe: ${out} ${copyHeadCmd} ${copyDllsCmd} & del *.obj ${cleanUpDllStuff}`;
+    const res =`mkdir ${outDir} & cl ${cpps} ${links} ${includeCmd} ${flags} ${dllFlag} /Fe: ${out} ${copyDllsCmd} & del *.obj ${cleanUpDllStuff}`;
+
+    const doCompile = () => exec(res, (_, stdout) => {
+        console.log(stdout);
+        const hFileOutDir = outDir + '\\' + outName + ".h";
+        if (hFile) writeFileSync(hFileOutDir, hFile);
+    });        
+
+    dryRun
+    ? console.log(res)
+    : doCompile();
 };
